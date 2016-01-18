ed684670875e96ab8d2177ff713b3783d384ed91
pwolschen <p.wolschen@gmail.com>
2016-01-18 16:27:19 -0500

    Added Classes

---
+++ src/VisionProcessor.java
@@ -0,0 +1,23 @@
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+
+
+public class VisionProcessor {
+	
+	public VisionProcessor(){
+		
+		
+	}
+	
+	public void GetTarget(){
+		
+		
+		
+	}
+	
+	
+
+}
